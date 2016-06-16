68f65a406d43a4fd94f93c8c0e3f35ea27d572a6
Gianni G <giangraziano@gmail.com>
2016-06-16 11:36:51 +0200

    simplified code

--- main/bot.go
+++ main/bot.go
@@ -9,55 +9,51 @@ func main() {
 
 	var bot telebot.Bot
 	conf := telebot.Configuration{
-		BotName: "EchoBot",
+		BotName: "NameBot",
 		ApiKey:  "123456789:AAAAAAAPIKEYHEREEEEEEE",
 	}
 
 	// bot start è una funzione che prende le configurazioni per collegarsi
-	// e una funziona (perché in go la funzione è un tipo)
-	// ricordarsi che in go le funzioni sono dei tipi
-	// comando defer (appena finisce il programma fa quello che gli dici di fare)
+	//in Go le funzioni sono dei tipi
 
-	//aggiungi un contatore che si incremente ogni volta che facciamo /next e cerchiamo il successivo
-	//cerchiamo nell'array di sendgooglesearchrequest(mess, i) quando i arriva al massimo answer prende valore "finished"
+	//bot.Start (c Configuration, resp Responder)
+	//dove type Responder func(telebot.TeleMessage) (string, erro)
 
-	bot.Start(conf, func(recMess telebot.TeleMessage) (string, error) {
-		var answer string
+	bot.Start(conf, echoFunction)
+}
 
-		user := recMess.From.Uname
-		if user == "" {
-			user = "everybody"
+func echoFunction(recMess telebot.TeleMessage) (string, error) {
+	var answer string
+
+	user := recMess.From.Uname
+	if user == "" {
+		user = "JohnDoe"
+	} else {
+		user = "@" + user
+	}
+	//parsare la stringa e prendere il comando e il messaggio
+	mess := recMess.Text
+
+	message := strings.SplitAfterN(mess, " ", 2)
+	cmd := strings.TrimSpace(message[0])
+
+	switch cmd {
+	case "/start":
+		answer = "Welcome to EchoBot!"
+	case "/help":
+		answer = "type /echo if wanna use echoBot in chat groups\n"
+	case "/echo":
+		if len(message) > 1 && message[1] != "" {
+			answer = "Hi " + user + " you typed: " + message[1] + "!"
 		} else {
-			user = "@" + user
+			answer = user + "you typed nothing!"
 		}
-
-		//parsare la stringa e prendere il comando e il messaggio
-		mess := recMess.Text
-
-		message := strings.SplitAfterN(mess, " ", 2)
-		cmd := strings.TrimSpace(message[0])
-
-		switch cmd {
-		case "/start":
-			answer = "Welcome!\ntype /echo if wanna use echoBot in chat groupstype\n/me reply your username\n/help for this message"
-		case "/help":
-			answer = "type /echo if wanna use echoBot in chat groups\ntype /hi for greet me"
-		case "/hi":
-			answer = "Hi " + user + "! :D\n"
-		case "/echo":
-			if len(message) > 1 && message[1] != "" {
-				answer = "Hi " + user + " you typed: " + message[1] + "!"
-			} else {
-				answer = user + "you typed nothing!"
-			}
-		default:
-			//no comamnd insert in message
-			if mess != "" {
-				answer = user + " typed: " + mess + "!"
-			} else {
-				answer = user + "you typed nothing!"
-			}
+	default: //if no comamnd insert in message
+		if mess != "" {
+			answer = user + " typed: " + mess + "!"
+		} else {
+			answer = user + "you typed nothing!"
 		}
-		return answer, nil
-	})
+	}
+	return answer, nil
 }
